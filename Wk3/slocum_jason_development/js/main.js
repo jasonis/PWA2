ea5519998c2573e9c6df576837912e432ab02d3b
jasonis <jislocum78@gmail.com>
2013-12-11 11:51:12 -0600

    development in progress

---
+++ Wk3/slocum_jason_development/js/main.js
@@ -0,0 +1,85 @@
+//         js
+
+$(document).ready(function(){
+    
+    
+    var loadLand = function(){
+    
+        $.ajax({
+            url: 'xhr/login.php',
+            type: 'post',
+            dataType: 'json',
+            success: function(response){
+                    var landing = response.landing;
+                    var html = '';
+                    var html = $.render(landing, "landingtemplate");
+                    
+                    $('template_landing').append(html);
+            }
+        });
+    };
+    
+    
+    var checkLogin = function(){
+	$.ajax({
+		url:'xhr/check_login.php',
+		type: 'get',
+		dataType: 'json',
+		success: function(r){
+			if(r.user){
+				loadApp();
+			}else{
+				loadLanding();
+				$('input, textarea').placeholder();
+			};
+		}
+	});
+    }
+    
+    
+    var init = function(){
+        $.get('templates/landing.html', function(){
+            var landing = $(htmlArg).find('#template_landing').html();
+            $.template('landingtemplate', landing);
+        });
+    };
+    
+    init();
+    
+    
+    $('button').on('click', function(e){
+        loadLand();
+        return false;
+    });
+    
+    
+    
+ /*
+    var loadLanding = function(){
+        var landing = $.get('templates/landing.html');
+        $.when(landing).then(function(h){
+            site.empty().append(h);
+        });
+    };
+    */
+    
+    var loadApp = function(){
+        var app = $.get('templates/app.html');
+        $.when(app).then(function(h){
+            site.empty().append(h);
+        });
+    };
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+});
