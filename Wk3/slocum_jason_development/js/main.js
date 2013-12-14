dfd9fdacd67add2660ffd6f2a743fea778a6b322
jasonis <jislocum78@gmail.com>
2013-12-13 21:00:24 -0600

    in progress

--- Wk3/slocum_jason_development/js/main.js
+++ Wk3/slocum_jason_development/js/main.js
@@ -1,92 +1,136 @@
 //         js
 
-$(document).ready(function(){
+$('document').ready(function(){
+    
+    var user;
     
     
-    
-    var loadLand = function(){
-        $.ajax({
-            url: 'xhr/login.php',
-            type: 'post',
-            dataType: 'json',
-            success: function(response){
-                    var landing = response.landing;
-                    var html = '';
-                    var html = $.render(landing, "landingtemplate");
-                    
-                    $('template_landing').append(html);
-            }
-        });
-    };
-    
-   
-    var checkLogin = function(){
-	$.ajax({
-		url:'xhr/check_login.php',
-		type: 'get',
-		dataType: 'json',
-		success: function(r){
-			if(r.user){
-				loadApp();
-			}else{
-				loadLanding();
-				$('input, textarea').placeholder();
-			};
-		}
-	});
-    }
-    
-    
-    $.ajax({
-	url: 'xhr/login.php',
-	data: {
-		username: user,
-		password: pass
-	},
-	type: 'post',
-	dataType: 'json',
-	success: function(response){
-		if(response.error){
-			showLoginError();
-		}else{
-			loadApp();
-		}
-	}
-		
+    $.ajaxSetup({
+        cache: false    
     });
-    
-    
-    
     
     var init = function(){
-        $.get('templates/landing.html', function(){
-            var landing = $(htmlArg).find('#template_landing').html();
-            $.template('landingtemplate', landing);
+        $.ajax({
+	    url:'xhr/check_login.php',
+	    type: 'get',
+	    dataType: 'json',
+	    success: function(response){
+                if(response.user){
+                    user = response.user;
+                    loadApp();
+                }else{
+                    loadLand();
+                }
+            }
+        });    
+    };
+    
+    
+    init(); 
+    
+    function loadLand(){
+        $.get('templates/landing.html', function(template){
+            var landingTemplate = template;
+            var mainPage = $('landingTemplate').find('#template_landing').html();
+            
+            $('#submit').click(function(){
+                var username = $('#username').val();
+                var password = $('#passwd').val();
+                
+                var valid = true;
+                
+                if(username.length === 0){
+                    valid = false;
+                }if(password.length <= 5){
+                    valid = false;
+                }else{
+                    var error = '<span class ="error">Invalid Username or Password</span>';
+                    $(error).appendTo('.signin');
+                    return;
+                }
+                
+                $.ajax({
+                    url: 'xhr/login.php',
+                    type: 'post',
+                    dataType: 'json',
+                    data: {
+                        'username': username,
+                        'password': password
+                    },
+                    success: function(response){
+                        if(response.user){
+                            user = response.user;
+                            init();
+                        }else{
+                            var error = '<span class = "error">Invalid Username or Password</span>';
+                            $(error).appendTo('.signin');
+                            return;
+                        }
+                    }
+                });
+            });
+            
+            // register new user section
+            $('#f_submit').click(function(){
+                var firstName = $('#f_name').val();
+                var lastName = $('#f_lastname').val();
+                var username = $('#username').val();
+                var password = $('#f_password').val();
+                var email = $('#f_email').val();
+                var valid = true;
+                
+                if(firstName.length === 0 || lastName.length === 0 || username.length === 0 || password.length === 0 ||email.length === 0){
+                    valid = false;
+                    var error = '<span class = "error">Please fill out the form completely.</span>';
+                    $(error).appendTo('#f_submit');
+                    return;
+                }
+                
+                $.ajax({
+                    url: 'xhr/register.php',
+                    type: 'post',
+                    dataType: 'json',
+                    data: {
+                        'firstname': firstName,
+                        'lastname': lastName,
+                        'username': username,
+                        'password': password,
+                        'email': email
+                    },
+                    success: function(response){
+                        if(response.user){
+                            loadApp();
+                        }else{
+                            var error = '<span class = "error">Please Try Again.</span>';
+                            $(error).appendTo('#f_submit');
+                            return;
+                        }
+                    }
+                });
+            });
+            
         });
     };
     
-    init();
+    function loadApp(){
+        $.get('templates/app.html', function(template){
+            var appTemplate = template;
+            var app = $('appTemplate').find('#template_app').html();
+            
+            $.template('appT', app);
+            var appHtml = $.render(user, 'appT');
+            $('app').html(appHtml);
+        });    
+    };
     
-    
-  
-    
-    $('#submit').on('click', function(e){
-        loadLand();
-        return false;
-    });
-    
-*/
-    
-    
-    
-    
-    
-    
+    win.on('click','#logout', function(){
+	$.get('xhr/logout.php', function(){
+		loadLand();
+	})
+	return false;
+    })
     
 
-    
-    
-    
-    
+
     
 });
